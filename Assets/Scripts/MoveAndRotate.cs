d125202ce2240c23e89779f6078385d3bc451690
hoduc <duc_ho90@yahoo.com>
2017-02-03 13:20:45 -0800

    Snow ball moving

--- Assets/Scripts/MoveAndRotate.cs
+++ Assets/Scripts/MoveAndRotate.cs
@@ -9,13 +9,19 @@ public class MoveAndRotate : MonoBehaviour {
 	public float speed = 1.0f;
 	public float rotationDegree = 90.0f;
 	public float scalingFactor = 0.1f;
+	public bool startMoving = false;
+	public GameObject Plane;
 
+	private Vector3 PlaneNormal;
 	private Vector3 targetPos;
-	private int targetIndex = 0;
+	private int targetIndex = -1;
 	private Dictionary<string,Func<Collider, bool>> colliderHandlers = new Dictionary<string,Func<Collider, bool>>();
 
 	bool PowerUpHandler(Collider other){
+		//other.enabled = false;
 		other.gameObject.SetActive(false);
+		//other.isTrigger = false;
+		//other.gameObject.GetComponent<MeshRenderer> ().enabled = false;
 		transform.localScale += new Vector3 (scalingFactor, scalingFactor, scalingFactor);
 		return true;
 	}
@@ -24,16 +30,25 @@ public class MoveAndRotate : MonoBehaviour {
 		colliderHandlers.Add ("powerup", PowerUpHandler);
 	}
 
+	Transform GetNextTransform(){
+		if (Targets.Length == 0)
+			return transform;
+		return Targets [++targetIndex%Targets.Length];
+	}
+
 	// Use this for initialization
 	void Start () {
 		SetUpColliderHandler ();
+		PlaneNormal = Plane.transform.TransformDirection (Plane.GetComponent<MeshFilter> ().mesh.normals [0]);
 		Debug.Log ("currentpos:" + transform.position);
-		targetPos = GetNewTargetPos(Targets[targetIndex]);
+		targetPos = GetNewTargetPos();
 		Debug.Log ("targetPos(new):" + targetPos);
 	}
 
-	Vector3 GetNewTargetPos(Transform targetTransform){
-		Vector3 targetPosProjected = Vector3.ProjectOnPlane (targetTransform.position, Vector3.up);
+	Vector3 GetNewTargetPos(){
+		Transform targetTransform = GetNextTransform();
+		Debug.Log ("target transform:" + targetTransform.name);
+		Vector3 targetPosProjected = Vector3.ProjectOnPlane (targetTransform.position, PlaneNormal);
 		return new Vector3 (targetPosProjected.x, transform.position.y, targetPosProjected.z);
 	}
 
@@ -47,10 +62,24 @@ public class MoveAndRotate : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
-		//transform.position = Vector3.Lerp(startPos, Target.position
-		if (transform.position == Vector3.MoveTowards (transform.position, targetPos, Time.deltaTime * speed))
-			targetPos = GetNewTargetPos (Targets[++targetIndex%Targets.Length]);
-		transform.position = Vector3.MoveTowards(transform.position, targetPos, Time.deltaTime * speed);
+		if (!startMoving)
+			return;
+		Debug.Log ("current index:" + targetIndex);
+
+		transform.position = Vector3.MoveTowards (transform.position, targetPos, Time.deltaTime * speed);
+		if (Vector3.Distance(transform.position,targetPos) < 0.5f){
+			Debug.Log ("reached target!!!");
+			targetPos = GetNewTargetPos ();	
+		}
+
+		//transform.position = Vector3.MoveTowards(transform.position, targetPos, Time.deltaTime * speed);
 		transform.Rotate (transform.right, Time.deltaTime * rotationDegree);
+	}
+
+	//handle state changing
+	void FixedUpdate(){
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			startMoving = !startMoving;
+		}
 	}
 }
